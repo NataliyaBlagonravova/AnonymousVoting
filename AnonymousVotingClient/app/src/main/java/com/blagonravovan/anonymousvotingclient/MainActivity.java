751a119f83a46bf1192463f66c0d12d66a9e3c34
Natalia Blagonravova <natalia.blagonravova@yotadevices.com>
2017-05-21 15:40:34 +0300

    Implemented sending the encrypted message from client and decrypting it on the server

--- AnonymousVotingClient/app/src/main/java/com/blagonravovan/anonymousvotingclient/MainActivity.java
+++ AnonymousVotingClient/app/src/main/java/com/blagonravovan/anonymousvotingclient/MainActivity.java
@@ -2,12 +2,67 @@ package com.blagonravovan.anonymousvotingclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.security.PublicKey;
 
 public class MainActivity extends AppCompatActivity {
+
+    private KeyStorage mKeyStorage;
+    private PublicKey mServerKey;
+
+    private EditText mMessageEditText;
+    private Button mSendMessageButton;
+
+    DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mKeyStorage = KeyStorage.getInstance(this);
+
+        mMessageEditText = (EditText) findViewById(R.id.message_to_server);
+        mSendMessageButton = (Button) findViewById(R.id.send_button);
+        mSendMessageButton.setEnabled(false);
+        mSendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = mMessageEditText.getText().toString();
+                String encryptMessage = EncryptionTools.encryptMessage(message, mServerKey);
+                mDatabaseReference.child("message").setValue(encryptMessage);
+            }
+        });
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        mDatabaseReference = database.getReference();
+        mDatabaseReference.child("server_public_key")
+                .addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String keyString = dataSnapshot.getValue(String.class);
+                mServerKey = KeyStorage.stringToPublicKey(keyString);
+                mSendMessageButton.setEnabled(true);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+
     }
 }
--- AnonymousVotingServer/app/src/main/java/com/blagonravovan/anonymousvotingserver/MainActivity.java
+++ AnonymousVotingServer/app/src/main/java/com/blagonravovan/anonymousvotingserver/MainActivity.java
@@ -2,12 +2,52 @@ package com.blagonravovan.anonymousvotingserver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private KeyStorage mKeyStorage;
+
+    private TextView mMessageTextView;
+
+    DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mKeyStorage = KeyStorage.getInstance(this);
+
+        mMessageTextView = (TextView) findViewById(R.id.message_from_client);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        mDatabaseReference = database.getReference();
+        mDatabaseReference.child("server_public_key").setValue(
+                KeyStorage.keyToString(mKeyStorage.getPublicKey()));
+
+        mDatabaseReference.child("message")
+                .addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String decryptMessage = dataSnapshot.getValue(String.class);
+                String message = EncryptionTools.decryptMessage(
+                        decryptMessage, mKeyStorage.getPrivateKey());
+                mMessageTextView.setText(message);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
