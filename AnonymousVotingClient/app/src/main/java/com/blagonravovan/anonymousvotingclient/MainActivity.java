82e0c308ffb042c3bc96b427502f6a50ee4e08e3
Natalia Blagonravova <natalia.blagonravova@yotadevices.com>
2017-05-21 18:07:18 +0300

    Implemented very simple voting protocol

--- AnonymousVotingClient/app/src/main/java/com/blagonravovan/anonymousvotingclient/MainActivity.java
+++ AnonymousVotingClient/app/src/main/java/com/blagonravovan/anonymousvotingclient/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RatingBar;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
     private EditText mMessageEditText;
     private Button mSendMessageButton;
 
+    private RatingBar mRatingCandidate1;
+    private RatingBar mRatingCandidate2;
+    private RatingBar mRatingCandidate3;
+    private RatingBar mRatingCandidate4;
+    private RatingBar mRatingCandidate5;
+
     DatabaseReference mDatabaseReference;
 
     @Override
@@ -32,13 +39,21 @@ public class MainActivity extends AppCompatActivity {
         mKeyStorage = KeyStorage.getInstance(this);
 
         mMessageEditText = (EditText) findViewById(R.id.message_to_server);
+
+        mRatingCandidate1 = (RatingBar) findViewById(R.id.rating1);
+        mRatingCandidate2 = (RatingBar) findViewById(R.id.rating2);
+        mRatingCandidate3 = (RatingBar) findViewById(R.id.rating3);
+        mRatingCandidate4 = (RatingBar) findViewById(R.id.rating4);
+        mRatingCandidate5 = (RatingBar) findViewById(R.id.rating5);
+
         mSendMessageButton = (Button) findViewById(R.id.send_button);
         mSendMessageButton.setEnabled(false);
         mSendMessageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String message = mMessageEditText.getText().toString();
-                String encryptMessage = EncryptionTools.encryptMessage(message, mServerKey);
+                String rating = getRatingString();
+                String encryptMessage = EncryptionTools.encryptMessage(message+ rating, mServerKey);
                 mDatabaseReference.child("message").setValue(encryptMessage);
             }
         });
@@ -59,10 +74,12 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
-
-
-
     }
-}
+
+    private String getRatingString(){
+     return "" +  Math.round(mRatingCandidate1.getRating()) +
+                  Math.round(mRatingCandidate2.getRating()) +
+                  Math.round(mRatingCandidate3.getRating()) +
+                  Math.round(mRatingCandidate4.getRating()) +
+                  Math.round(mRatingCandidate5.getRating());
+}}
--- AnonymousVotingServer/app/src/main/java/com/blagonravovan/anonymousvotingserver/MainActivity.java
+++ AnonymousVotingServer/app/src/main/java/com/blagonravovan/anonymousvotingserver/MainActivity.java
@@ -2,6 +2,7 @@ package com.blagonravovan.anonymousvotingserver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -10,11 +11,25 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private KeyStorage mKeyStorage;
 
     private TextView mMessageTextView;
+
+    private TextView mRating1TextView;
+    private TextView mRating2TextView;
+    private TextView mRating3TextView;
+    private TextView mRating4TextView;
+    private TextView mRating5TextView;
+
+    private TextView[] mRatingTextViews;
+
+    private int[] mRating;
 
     DatabaseReference mDatabaseReference;
 
@@ -25,7 +40,20 @@ public class MainActivity extends AppCompatActivity {
 
         mKeyStorage = KeyStorage.getInstance(this);
 
-        mMessageTextView = (TextView) findViewById(R.id.message_from_client);
+        mRating1TextView = (TextView) findViewById(R.id.rating1);
+        mRating2TextView = (TextView) findViewById(R.id.rating2);
+        mRating3TextView = (TextView) findViewById(R.id.rating3);
+        mRating4TextView = (TextView) findViewById(R.id.rating4);
+        mRating5TextView = (TextView) findViewById(R.id.rating5);
+
+        mRatingTextViews = new TextView[]{
+                mRating1TextView,
+                mRating2TextView,
+                mRating3TextView,
+                mRating4TextView,
+                mRating5TextView
+        };
+
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         mDatabaseReference = database.getReference();
@@ -34,20 +62,44 @@ public class MainActivity extends AppCompatActivity {
 
         mDatabaseReference.child("message")
                 .addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String decryptMessage = dataSnapshot.getValue(String.class);
-                String message = EncryptionTools.decryptMessage(
-                        decryptMessage, mKeyStorage.getPrivateKey());
-                mMessageTextView.setText(message);
-            }
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String decryptBulletin = dataSnapshot.getValue(String.class);
+                        String bulletin  = EncryptionTools.decryptMessage(
+                                decryptBulletin, mKeyStorage.getPrivateKey());
+                        Log.d(TAG, "New bulletin: " + bulletin);
+                        if (bulletin.length() == 5) {
+                            updateRating(bulletin);
+                            showRating();
+                        }
+                    }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
 
-            }
-        });
+                    }
+                });
 
+        mRating = new int[]{0, 0, 0, 0, 0};
 
+        showRating();
+    }
+
+    private void showRating(){
+        for (int i = 0; i < mRatingTextViews.length; ++i){
+            mRatingTextViews[i].setText(String.valueOf(mRating[i]));
+        }
+    }
+
+    private void updateRating(String bulletin){
+        Log.d(TAG, bulletin);
+        int magicNumber = Integer.valueOf(bulletin);
+
+        int divider = 10000;
+        for (int i = 0; i < mRating.length; ++i) {
+            mRating[i] += magicNumber / divider;
+            magicNumber %= divider;
+            divider /= 10;
+        }
     }
 }
